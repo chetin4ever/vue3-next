7b9c43c22458dd3f6a33dcf9208b48b8496e5bd6
chetin4ever <hetan009@gmail.com>
2021-01-25 15:20:45 +0530

    creating reuseable modal

---
+++ src/components/RModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <section
+    @click="close"
+    class="z-20 w-full h-full bg-red-900 opacity-50 fixed top-0 left-0"
+  ></section>
+  <div class="absolute inset-0 text-gray-500">
+    <div class="flex h-full">
+      <div class="z-30 m-auto p-2 rounded bg-white w-1/3">
+        <div class="p-2 border-2 border-pink-200">
+          <h1>
+            <slot name="title"></slot>
+          </h1>
+          <main>
+            <slot name="body"></slot>
+          </main>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup(_, { emit }) {
+    function close() {
+      emit("close");
+    }
+    return { close };
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/ReusableModal.vue
+++ src/views/ReusableModal.vue
@@ -1,7 +1,40 @@
-<template>reausable</template>
+<template>
+  <div class="flex w-full">
+    <div class="m-auto">
+      <h1 class="text-2xl my-2">Reuseable Modal</h1>
+      <r-modal v-if="isModal1Open" @close="isModal1Open = false">
+        <template #title>
+          <h1 class="text-2xl text-red-700">Modal1</h1>
+        </template>
+        <template #body
+          ><p class="text-2xl text-red-700">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum magnam
+            voluptatibus explicabo vitae, voluptatum ea recusandae error
+            provident, repu
+          </p></template
+        >
+      </r-modal>
+      <r-modal v-if="isModal2Open" @close="isModal2Open = false">
+        <h1 class="text-2xl text-red-700">Modal 2</h1>
+      </r-modal>
+      <button @click="isModal1Open = true">Modal 1</button>
+      <button @click="isModal2Open = true">Modal 2</button>
+    </div>
+  </div>
+</template>
 
 <script>
-export default {};
+import { ref } from "vue";
+import RModal from "../components/RModal.vue";
+export default {
+  components: { RModal },
+  setup() {
+    const isModal1Open = ref(false);
+    const isModal2Open = ref(false);
+
+    return { isModal1Open, isModal2Open };
+  },
+};
 </script>
 
 <style>
